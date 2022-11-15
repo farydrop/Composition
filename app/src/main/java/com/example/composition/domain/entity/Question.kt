1af0090ee0c8cd53021f3743a84b3b982ee41dd7
farida <farida.hairullina5@gmail.com>
2022-11-15 21:20:25 +0100

    Add GameViewModel

--- app/src/main/java/com/example/composition/domain/entity/Question.kt
+++ app/src/main/java/com/example/composition/domain/entity/Question.kt
@@ -4,4 +4,7 @@ data class Question (
     val sum: Int,              //значение суммы, которое отображается в кружке
     val visibleNumber: Int,    //видимое число,которое отображается в левом квадрате
     val options: List<Int>     //варианты ответов
-)
+) {
+    val rightAnswer: Int
+        get() = sum - visibleNumber
+}
